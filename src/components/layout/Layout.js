07d928c63cc83574797f5233462a8efc1c6c3acb
Dungne <bed209fit@gmail.com>
2025-10-28 09:54:29 +0700

    fix header

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-1 bg-white">{children}</main>
+      <main className="flex-1 bg-white mt-[30px]">{children}</main>
       <Footer />
       <ChatWidget />
     </div>
